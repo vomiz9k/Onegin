1be17ff3f350b691c0e302cf5ba0083bdf811dcf
vomiz9k <55029930+vomiz9k@users.noreply.github.com>
2019-09-17 14:03:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -13,8 +13,7 @@ const int FILENAMESIZE = 50, SPAMSIZE = 10;
 
 int main()
 {
-    
-    char c;
+    char c = 'n';
     printf ("Make UNIT TESTS? (Y/N): ");
     c = getchar();
     printf ("\n");
@@ -24,19 +23,19 @@ int main()
     char fin_name[FILENAMESIZE] = "", fout_name[FILENAMESIZE] = "";
     get_file_names (fin_name, fout_name);
 
-    FILE *fin = fopen (fin_name, "r"), *fout = fopen (fout_name, "w");
-    int size_of_file = file_size (fin);
+    FILE *fin = fopen(fin_name, "r"), *fout = fopen (fout_name, "w");
+    int size_of_file = file_size(fin);
 
-    char* onegin = (char*) file_read (fin, &size_of_file);
-    int string_count = get_string_count (onegin, size_of_file);
+    char* onegin = (char*) file_read(fin, &size_of_file);
+    int string_count = get_string_count(onegin, size_of_file);
 
-    struct indexes *index = (indexes*) calloc (string_count, sizeof(indexes));
-    get_str_index (onegin, index, string_count, size_of_file);
+    struct indexes *index = (indexes*) calloc(string_count, sizeof(indexes));
+    get_str_index(onegin, index, string_count, size_of_file);
 
-    print_all_to (fout, index, string_count, onegin, SPAMSIZE);
+    print_all_to(fout, index, string_count, onegin, SPAMSIZE);
 
-    free (index);
-    free (onegin);
+    free   (index);
+    free   (onegin);
     fclose (fin);
     fclose (fout);
 
