c2f1c00034e7a66bc3d4620c7e277005059de637
vomiz9k <55029930+vomiz9k@users.noreply.github.com>
2019-09-17 13:39:12 +0300

    upd 1.1

--- main.c
+++ main.c
@@ -4,40 +4,43 @@
 #include "stringsort.c"
 #include "unit_tests.c"
 
-#define KERN "kernigan.txt"
-#define ONEG "onegin.txt"
-#define OUTF "output.txt"
-#define FILENAMESIZE 50
-#define SPAMSIZE 10
 
+const char KERN[] = "kernigan.txt", ONEG[] = "onegin.txt", OUTF[] = "output.txt";
+const int FILENAMESIZE = 50, SPAMSIZE = 10;
 
 /*!This program sorts strings in input file.*/
 
 
 int main()
 {
-    unit_test();
+    
+    char c;
+    printf ("Make UNIT TESTS? (Y/N): ");
+    c = getchar();
+    printf ("\n");
+    if (c == 'Y' || c == 'y')
+        unit_test();
 
-    char fin_name[FILENAMESIZE]="", fout_name[FILENAMESIZE]="";
-    get_file_names(fin_name, fout_name);
+    char fin_name[FILENAMESIZE] = "", fout_name[FILENAMESIZE] = "";
+    get_file_names (fin_name, fout_name);
 
-    FILE *fin = fopen(fin_name, "r"), *fout = fopen(fout_name, "w");
-    int size_of_file = file_size(fin);
+    FILE *fin = fopen (fin_name, "r"), *fout = fopen (fout_name, "w");
+    int size_of_file = file_size (fin);
 
-    char* onegin = (char*)file_read(fin, &size_of_file);
-    int string_count = get_string_count(onegin, size_of_file);
+    char* onegin = (char*) file_read (fin, &size_of_file);
+    int string_count = get_string_count (onegin, size_of_file);
 
-    struct indexes *index = (indexes*)calloc(string_count, sizeof(indexes));
-    get_str_index(onegin, index, string_count, size_of_file);
+    struct indexes *index = (indexes*) calloc (string_count, sizeof(indexes));
+    get_str_index (onegin, index, string_count, size_of_file);
 
-    print_all_to(fout, index, string_count, onegin, SPAMSIZE);
+    print_all_to (fout, index, string_count, onegin, SPAMSIZE);
 
-    free(index);
-    free(onegin);
-    fclose(fin);
-    fclose(fout);
+    free (index);
+    free (onegin);
+    fclose (fin);
+    fclose (fout);
 
-    printf("Success! Check the %s.", fout_name);
+    printf ("Success! Check the %s.", fout_name);
 
 }
 
